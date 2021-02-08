3e1b32c5dc797b37c8a6b0675b62fecd64a6492c
Rayland Cheng <raylandcheng@solomon-systech.com>
2021-02-08 14:56:08 +0800

    tasks, add tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask"
+import {useState} from 'react'
+
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {id:1, text:"ABCE", day:"Monday 1st February 2021", reminder: true},
+        {id:2, text:"DEF", day:"Tuesday 2nd February 2021", reminder: true},
+        {id:3, text:"HIJ", day:"Wednesday 3rd February 2021", reminder: true},
+        {id:4, text:"KLM", day:"Thursday 4th February 2021", reminder: true},
+    ]
+    );
+  
+  const addTask = (task) => {
+    // console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  };
+
+  const deleteTask = (id) => {
+    // console.log('delete', id)
+    setTasks(tasks.filter( (task) => task.id !== id))
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map( (task) => task.id === id ? { ...task, reminder: !task.reminder} : task))
+  }
+
   return (
-    <div className="App">
+    <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (
+      <Tasks tasks ={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Task from './Task'
+// const tasks = [
+//     {id:1, text:"ABCE", day:"Monday 1st February 2021", reminder: true},
+//     {id:2, text:"DEF", day:"Tuesday 2nd February 2021", reminder: true},
+//     {id:3, text:"HIJ", day:"Wednesday 3rd February 2021", reminder: true},
+//     {id:4, text:"KLM", day:"Thursday 4th February 2021", reminder: true},
+// ]
 
-function Tasks() {
+
+
+function Tasks({ tasks, onDelete, onToggle}) {
+    
+
+
     return (
         <div>
-            
+            {tasks.map((task) => (
+          
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+            ))}
         </div>
     )
 }
